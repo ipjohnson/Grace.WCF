e82fbf6fa3d66b02e16fb547bdf94f91a5031915
ipjohnson <ipjohnson123@yahoo.com>
2017-05-22 10:12:51 -0600

    fixing formatting

--- src/Grace.WCF/Extensions/GraceServiceHostFactory.cs
+++ src/Grace.WCF/Extensions/GraceServiceHostFactory.cs
@@ -8,21 +8,21 @@ namespace Grace.WCF.Extensions
     /// Service host factory
     /// </summary>
 	public class GraceServiceHostFactory : GraceBaseServiceHostFactory
-	{
+    {
         /// <summary>
         /// Default constructor, uses DefaultExportLocator.Instance
         /// </summary>
 		public GraceServiceHostFactory() : base(DefaultExportLocator.Instance)
-		{
-		}
+        {
+        }
 
         /// <summary>
         /// Constructor that takes export locator
         /// </summary>
         /// <param name="exportLocator"></param>
 		public GraceServiceHostFactory(IExportLocatorScope exportLocator) : base(exportLocator)
-		{
-		}
+        {
+        }
 
         /// <summary>
         /// Creates new service host
@@ -31,8 +31,8 @@ namespace Grace.WCF.Extensions
         /// <param name="baseAddresses"></param>
         /// <returns></returns>
 		protected override ServiceHost CreateServiceHostInstance(Type serviceType, Uri[] baseAddresses)
-		{
-			return new ServiceHost(serviceType, baseAddresses);
-		}
-	}
+        {
+            return new ServiceHost(serviceType, baseAddresses);
+        }
+    }
 }
--- src/Grace.WCF/Extensions/GraceWebServiceHostFactory.cs
+++ src/Grace.WCF/Extensions/GraceWebServiceHostFactory.cs
@@ -9,31 +9,31 @@ namespace Grace.WCF.Extensions
     /// Factory for web
     /// </summary>
 	public class GraceWebServiceHostFactory : GraceBaseServiceHostFactory
-	{
+    {
         /// <summary>
         /// Default web host factory, uses DefaultExportLocator.Instance
         /// </summary>
 		public GraceWebServiceHostFactory() : base(DefaultExportLocator.Instance)
-		{
-		}
+        {
+        }
 
         /// <summary>
         /// Constructor that takes export locator
         /// </summary>
         /// <param name="exportLocator"></param>
 		public GraceWebServiceHostFactory(IExportLocatorScope exportLocator) : base(exportLocator)
-		{
-		}
+        {
+        }
 
-	    /// <summary>
-	    /// Create service host isntance
-	    /// </summary>
-	    /// <param name="serviceType"></param>
-	    /// <param name="baseAddresses"></param>
-	    /// <returns></returns>
-	    protected override ServiceHost CreateServiceHostInstance(Type serviceType, Uri[] baseAddresses)
-		{
-			return new WebServiceHost(serviceType, baseAddresses);
-		}
-	}
+        /// <summary>
+        /// Create service host isntance
+        /// </summary>
+        /// <param name="serviceType"></param>
+        /// <param name="baseAddresses"></param>
+        /// <returns></returns>
+        protected override ServiceHost CreateServiceHostInstance(Type serviceType, Uri[] baseAddresses)
+        {
+            return new WebServiceHost(serviceType, baseAddresses);
+        }
+    }
 }
